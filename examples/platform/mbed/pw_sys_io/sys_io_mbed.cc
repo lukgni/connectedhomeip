2670d4934f6ccf6d70e065a4dc32b1fd5e4ab64f
ATmobica <artur.tynecki@mobica.com>
2021-06-16 12:02:30 +0000

    Add CONFIG_CHIP_PW_RPC config flag Pigweed RPC support Add pw_rpc lib buidling files to mbed config Add pw_sys integration files to examples mbed platform

---
+++ examples/platform/mbed/pw_sys_io/sys_io_mbed.cc
@@ -0,0 +1,60 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <cassert>
+#include <cinttypes>
+
+#include "pw_sys_io/sys_io.h"
+
+extern "C" void pw_sys_io_Init() {}
+
+namespace pw::sys_io {
+
+Status ReadByte(std::byte * dest)
+{
+    if (!dest)
+    {
+        return Status::InvalidArgument();
+    }
+
+    return OkStatus();
+}
+
+Status WriteByte(std::byte b)
+{
+    return OkStatus();
+}
+
+// Writes a string using pw::sys_io, and add newline characters at the end.
+StatusWithSize WriteLine(const std::string_view & s)
+{
+    size_t chars_written  = 0;
+    StatusWithSize result = WriteBytes(std::as_bytes(std::span(s)));
+    if (!result.ok())
+    {
+        return result;
+    }
+    chars_written += result.size();
+
+    // Write trailing newline.
+    result = WriteBytes(std::as_bytes(std::span("\r\n", 2)));
+    chars_written += result.size();
+
+    return StatusWithSize(result.status(), chars_written);
+}
+
+} // namespace pw::sys_io
